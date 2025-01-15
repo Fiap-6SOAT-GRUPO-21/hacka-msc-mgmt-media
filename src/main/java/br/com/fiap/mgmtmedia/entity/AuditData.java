55ee17cb32f21c79d7e13bb76fc62dd199c7a044
Guilherme Costa Sales <guilhermecosales@gmail.com>
2025-01-15 16:01:09 -0300

    fix: add @MappedSuperclass

--- src/main/java/br/com/fiap/mgmtmedia/entity/AuditData.java
+++ src/main/java/br/com/fiap/mgmtmedia/entity/AuditData.java
@@ -1,10 +1,12 @@
 package br.com.fiap.mgmtmedia.entity;
 
+import jakarta.persistence.MappedSuperclass;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
 import java.time.LocalDateTime;
 
+@MappedSuperclass
 public abstract class AuditData {
 
     @CreationTimestamp
