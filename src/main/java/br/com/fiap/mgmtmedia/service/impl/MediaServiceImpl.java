d8904730d88ff9b3a8468f76e4af7907d8102a96
Guilherme Costa Sales <guilhermecosales@gmail.com>
2025-01-15 16:02:56 -0300

    feat: save storage path and improve exception

--- src/main/java/br/com/fiap/mgmtmedia/service/impl/MediaServiceImpl.java
+++ src/main/java/br/com/fiap/mgmtmedia/service/impl/MediaServiceImpl.java
@@ -1,14 +1,16 @@
 package br.com.fiap.mgmtmedia.service.impl;
 
-import br.com.fiap.mgmtmedia.sqs.model.MediaMessage;
-import br.com.fiap.mgmtmedia.sqs.producer.SQSProducer;
 import br.com.fiap.mgmtmedia.entity.MediaMetadata;
 import br.com.fiap.mgmtmedia.enumerated.MediaStatus;
+import br.com.fiap.mgmtmedia.exception.custom.MediaException;
 import br.com.fiap.mgmtmedia.repository.MediaRepository;
 import br.com.fiap.mgmtmedia.service.MediaService;
 import br.com.fiap.mgmtmedia.service.S3Service;
+import br.com.fiap.mgmtmedia.sqs.model.MediaMessage;
+import br.com.fiap.mgmtmedia.sqs.producer.SQSProducer;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
@@ -21,9 +23,12 @@ import java.io.IOException;
 @RequiredArgsConstructor
 public class MediaServiceImpl implements MediaService {
 
+    @Value("${variables.aws.bucket-name}")
+    private String bucketName;
+
     private final MediaRepository mediaRepository;
 
-    private final SQSProducer SQSProducer;
+    private final SQSProducer sqsProducer;
 
     private final S3Service s3Service;
 
@@ -33,11 +38,12 @@ public class MediaServiceImpl implements MediaService {
 
         try {
             String fileName = mediaFile.getOriginalFilename();
+
             s3Service.putObject(fileName, mediaFile.getBytes());
-            log.info("File uploaded successfully.");
 
             MediaMetadata mediaMetadata = MediaMetadata.builder()
                     .status(MediaStatus.UPLOADED)
+                    .storagePath(buildStoragePath(fileName))
                     .userReference("userReference")
                     .build();
 
@@ -48,12 +54,18 @@ public class MediaServiceImpl implements MediaService {
                     .storagePath(fileName)
                     .userReference("userReference")
                     .build();
-            SQSProducer.publishMessage(mediaMessage);
+
+            sqsProducer.publishMessage(mediaMessage);
 
             return storedMediaMetadata;
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            log.error("Error uploading media file: {}", e.getMessage(), e);
+            throw new MediaException("Error uploading media file");
         }
+    }
+
+    private String buildStoragePath(final String fileName) {
+        return "s3://" + bucketName + "/" + fileName;
     }
 
     @Override
